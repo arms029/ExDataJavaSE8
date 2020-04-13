d0aec4c915b0357a131c48af0544796b3d9a808b
Ricardo <46171495+arms029@users.noreply.github.com>
2020-04-13 03:24:59 -0300

    Atualizando comentários

--- TestaData.java
+++ TestaData.java
@@ -15,47 +15,56 @@ public class TestaData {
         for (String arg : args) {
             System.out.println(arg);
         }
-
+        
+        //Cria um objeto do tipo LocalDate
         LocalDate d = LocalDate.now(); //yyyy-MM-dd
+        //Cria um objeto do tipo LocalTime
         LocalTime t = LocalTime.now(); //hh:mm:ss.ms
+        ////Cria um objeto do tipo LocalDateTime
         LocalDateTime dt = LocalDateTime.now(); //yyyy-MM-dd(T)hh:mm:ss.ms
 
         System.out.println(d);
         System.out.println(t);
         System.out.println(dt);
-
+        
+        //Criando data usando fusos horários diferentes
         LocalTime t2 = LocalTime.now(ZoneId.of("America/Chicago"));
         LocalTime t3 = LocalTime.now(ZoneId.of("Africa/Cairo"));
         System.out.println(t2);
         System.out.println(t3);
-
+        
+        //Cria um LocalTime passando o horário do dia como parâmetro
         LocalTime meioDia = LocalTime.of(12,0,0);
         System.out.println(meioDia);
-
+        
+        //Cria um LocalDate passando a respectiva data como parâmetro
         LocalDate natal2014 = LocalDate.of(2014, Month.DECEMBER, 25);
         System.out.println(natal2014);
-
+        
+        //Cria um LocalDateTime passando a data e o horário do dia como parâmetro
         LocalDateTime data = LocalDateTime.of(2020, 12, 31, 23, 59); //ano bissexto, 366dias
-        System.out.println(data.getDayOfYear());
+        System.out.println(data.getDayOfYear()); //pega o dia em relação ao ano (1-365)
         System.out.println(data.minusHours(2)); //subtrai horas
-        System.out.println(data.get(ChronoField.MONTH_OF_YEAR)); 
-        System.out.println(data.get(ChronoField.DAY_OF_WEEK));
+        System.out.println(data.get(ChronoField.MONTH_OF_YEAR)); //pega o mês do ano
+        System.out.println(data.get(ChronoField.DAY_OF_WEEK)); //pega o dia da semana
         System.out.println(data.getDayOfWeek());
-
+        
+        
         MonthDay day1 = MonthDay.of(1,1); //1/jan
         MonthDay day2 = MonthDay.of(1,2); //2/jan
-
+    
+        //Operações lógicas envolvendo datas
         System.out.println(day1.isAfter(day2)); //false
         System.out.println(day1.isBefore(day2)); // true
         System.out.println(day1.equals(day1)); //true
-
+        
+        //Objetos do tipo LocalDate são imutáveis
         LocalDate dataImutavel = LocalDate.of(1991, 12, 25);
-
         LocalDate dataModificada = dataImutavel.withMonth(Month.JULY.getValue());
         System.out.println(dataModificada);
         System.out.println(dataImutavel);
-
-        LocalDate dataModificada2 = dataImutavel.plusMonths(2).minusDays(25);
+        
+        LocalDate dataModificada2 = dataImutavel.plusMonths(2).minusDays(25); //cálculo envolvendo datas
         System.out.println(dataModificada2);
 
         LocalDate dataModificada3 = dataImutavel.minus(4, ChronoUnit.DAYS);
